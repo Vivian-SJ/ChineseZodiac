fb9f7ebf88dfc51bb05aaca95d9c3b758cee0476
151250182 <151250182@smail.nju.edu.cn>
2017-09-02 17:11:04 +0800

    easy logic and finish the first program

--- ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var year: UITextField!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    let offset = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        year.resignFirstResponder()
+    }
 
 
+    @IBAction func okDidTap(_ sender: Any) {
+        year.resignFirstResponder()
+        
+        if let yearOfBirth = Int(year.text!) {
+            var imageNum = (yearOfBirth - offset) % 12
+            image.image = UIImage(named: String(imageNum))
+        }
+    }
 }
 
